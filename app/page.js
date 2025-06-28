9e311660521be489c03f67d4f4f93324657eed2a
realhardik18 <realhardik18@gmail.com>
2025-06-28 16:26:48 +0530

    basic skeleton

--- app/page.js
+++ app/page.js
@@ -4,7 +4,24 @@ export default function Home() {
     <div className="h-screen bg-black">
       <div className="pl-5 pt-5">
         <div className="text-6xl">yo i'm hrdk</div>
-        <Links/>
+        <Links/>   
+        <div className="pt-8 text-xl leading-relaxed max-w-2xl">
+            <p>i'm a 19 y/o builder actively shipping and learning</p>
+            <p>building projects in AI/ML, full-stack, & automation</p>
+            <p>trying to write code that pays rent (eventually)</p>
+            <p>running grindset.club & babysitting mykozu.xyz rn</p>
+        </div>              
+        <div className="pt-8 text-xl leading-relaxed max-w-2xl">
+          <p>freelanced on 20+ tech gigs and builds</p>
+          <p>won 5 hackathons solving real dev problems</p>
+          <p>prev AI/ML intern, worked at getblue.ai</p>
+          <p>check out some of my projects <a href="/" target="_blank" className="underline">here</a></p>
+        </div>
+        <div className="pt-8 text-xl leading-relaxed max-w-2xl">
+          <p>i love to hit the gym and play sports</p>          
+          <p>open for gigs and internships</p>
+          <p><a href="https://www.timeanddate.com/worldclock/india/bengaluru" target="_blank" className="underline">based in blr</a> | <a href="/" target="_blank" className="underline">get in touch</a></p>
+        </div>           
       </div>
     </div>
   );
