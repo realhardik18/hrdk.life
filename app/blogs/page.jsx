09a77cf3cebeb27df8e88d872345a2e4c226ce21
realhardik18 <realhardik18@gmail.com>
2025-08-30 02:25:35 +0530

    added sample blog page

---
+++ app/blogs/page.jsx
@@ -0,0 +1,7 @@
+export default function Blogs(){
+    return(
+        <div className="flex justify-center items-center h-screen">
+            <p className="text-4xl">coming very soon</p>
+        </div>
+    )
+}
