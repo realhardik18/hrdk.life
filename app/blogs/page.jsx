396914e2e746fcb94c5c7be2d3d9e0998a38181d
Hardik <realhardik18@gmail.com>
2026-07-13 13:50:51 +0530

    start over

--- app/blogs/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Blogs(){
-    return(
-        <div className="flex justify-center items-center h-screen">
-            <p className="text-4xl">coming very soon</p>
-        </div>
-    )
-}
